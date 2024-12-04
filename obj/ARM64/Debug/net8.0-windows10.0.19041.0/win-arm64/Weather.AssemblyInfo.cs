9995e55cb5546db7c26422dc21fc5b515fce5a1c
YuruDeveloper <vkgkvk8@gmail.com>
2024-12-04 18:21:33 +0900

    readme change

--- obj/ARM64/Debug/net8.0-windows10.0.19041.0/win-arm64/Weather.AssemblyInfo.cs
+++ obj/ARM64/Debug/net8.0-windows10.0.19041.0/win-arm64/Weather.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Weather")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6620d4c8d5a7af7cda7894cc180ae5485224504c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f4d137d5a6d4e607f29db155301bb6b4c1c85c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Weather")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Weather")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
